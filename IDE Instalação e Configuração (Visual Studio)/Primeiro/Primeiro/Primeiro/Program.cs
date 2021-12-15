c402ddf07075096327d5b14c339d189326a18f6d
Edu-Antunes <eduardo.antunes85@hotmail.com>
2021-12-14 22:04:37 -0300

    Inclusão IDE Instalação e Configuração (Visual Studio)

---
+++ IDE Instalação e Configuração (Visual Studio)/Primeiro/Primeiro/Primeiro/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Primeiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            var a = "Hello ";
+            var b = "Word";
+
+            Console.WriteLine(a+b);
+        }
+    }
+}
