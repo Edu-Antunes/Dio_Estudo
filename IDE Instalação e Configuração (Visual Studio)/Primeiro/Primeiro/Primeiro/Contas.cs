95d81b2670a42925d4738c6940ddbfd9b246bfe0
Edu-Antunes <eduardo.antunes85@hotmail.com>
2021-12-15 07:18:00 -0300

    Criando teste

---
+++ IDE Instalação e Configuração (Visual Studio)/Primeiro/Primeiro/Primeiro/Contas.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contas
+
+{
+    public class Contas
+    {
+        public int Soma(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtracao(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Divicao(int a, int b)
+        {
+            return a / b;
+        }
+    
+
+        public int Multiplicacao(int a, int b)
+        {
+        return a * b;
+        }
+
+}
+}
+
+
+
