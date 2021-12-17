c3bb5c813bd0168c51d197fb4a25cf3aaa7f7d08
Eduardo Antunes <eduardo.antunes85@hotmail.com>
2021-12-16 21:44:29 -0300

    Finalizando a introdução C# e .NET

---
+++ IDE Instalação e Configuração (Visual Studio)/CSharp/CSharp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Interface;
+using Enum;
+
+
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pessoa1 = (Pessoas)0;
+            Pessoas pessoa2 = Pessoas.Edu;
+            Pessoas pessoa3 = Pessoas.Carlos;
+            var pessoa4 = (Pessoas)5;
+
+
+            Pessoa persion = new Pessoa();
+
+            persion.Nome = "Edu";
+            persion.Idade = 18;
+            persion.Estado = "SP";
+
+            var persion2 = new Pessoa();
+            persion2.Nome = "Carlos";
+            persion2.Idade = 30;
+            persion2.Estado = "PA";
+
+            Animal animal = new Animal();
+            animal.Nome = "Toto";
+            animal.Nome = "Carlos";
+            animal.Especie = "Cachorro";
+
+
+            Console.WriteLine(persion.Idade);
+            Console.WriteLine(animal.Especie);
+            Console.WriteLine(pessoa3); 
+        }
+    }
+}
