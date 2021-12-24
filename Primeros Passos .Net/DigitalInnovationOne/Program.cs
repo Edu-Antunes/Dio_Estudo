73520a20e9b2e23787e51f1fef2f219cf4a93c07
Edu-Antunes <eduardo.antunes85@hotmail.com>
2021-12-23 22:10:45 -0300

    Criação da pasta Primeiros passos DOTNET

---
+++ Primeros Passos .Net/DigitalInnovationOne/Program.cs
@@ -0,0 +1,18 @@
+﻿ using System;
+
+namespace DigitalInnovationOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numerosDeVezes = 9;
+
+            for (int i = 0; i < numerosDeVezes; i++){
+                Console.WriteLine($"Bem vindo ao curso de .Net! {i}");
+            }
+
+            
+        }
+    }
+}
